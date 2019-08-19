6b1bc47c352436faf2f0c60da0a0e52433867496
SergeiLutsev <snlutsev@outlook.com>
2019-08-19 09:16:55 -0400

    db conection changed

--- Eclipse/HiperDemo/src/main/java/com/sergei/HiperDemo/App.java
+++ Eclipse/HiperDemo/src/main/java/com/sergei/HiperDemo/App.java
@@ -1,8 +1,5 @@
 package com.sergei.HiperDemo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
